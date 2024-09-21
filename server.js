455addc6c75ce47caeea779fe8b9b262b7fcfb2a
Syed Talha Ahmed <41064991+Talhaverse@users.noreply.github.com>
2024-09-21 14:24:53 +0500

    simple routing

--- server.js
+++ server.js
@@ -2,10 +2,29 @@ import http from 'http';
 const PORT = process.env.PORT;
 
 const server = http.createServer((req, res)=>{
-   
+try {
 
-    res.writeHead(200,{ 'Content-Type': 'text/html'});
-    res.end('<h1>hello world</h1>');     
+
+//CHeck if get  
+if (req.method === 'GET'){
+    if (req.url === '/') {
+        res.writeHead(200,{ 'Content-Type': 'text/html'});
+        res.end('<h1>hello world</h1>');  
+       } else if (req.url === '/about') {
+        res.writeHead(200,{ 'Content-Type': 'text/html'});
+        res.end('<h1>about</h1>');  
+       } else {
+        res.writeHead(404,{ 'Content-Type': 'text/html'});
+        res.end('<h1>404 page not found</h1>');  
+       }
+}else {
+    throw new Error('Invalid request method');
+}
+} catch (error) {
+    res.writeHead(500,{ 'Content-Type': 'text/html'});
+        res.end('<h1>Server error</h1>');  
+}
+
 })
 
 server.listen(8000, ()=>{
