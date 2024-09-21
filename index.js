4cdf166645cf88d903f7d6b457703082ceab7677
Syed Talha Ahmed <41064991+Talhaverse@users.noreply.github.com>
2024-09-21 13:22:56 +0500

    learning common js and module js commands

---
+++ index.js
@@ -0,0 +1,12 @@
+import  getPosts, {getPostLength} from './postController.js';
+
+console.log(getPosts()); 
+console.log(getPostLength());
+
+
+
+
+// const {generateRandomNumber,celciusToFahrenheit} = require('./utils');
+
+// console.log(`Random number: ${generateRandomNumber()}`); // Random number: 42
+// console.log(`Celcius in Fahrenheit is: ${celciusToFahrenheit(0)}`); // 32°C in Fahrenheit is: 89.6
